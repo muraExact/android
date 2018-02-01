6e32ee9f297d1895e1db6e65158e30618900bdd4
muraExact <60900114001@uin-alauddin.ac.id>
2018-02-01 05:27:29 -0800

    ver 1.0.0

---
+++ BudayakuV2-master/app/src/main/java/com/example/admin/budayakuv2/JawaTengah.java
@@ -0,0 +1,15 @@
+package com.example.admin.budayakuv2;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mura on 11/11/1996.
+ */
+public class JawaTengah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jawatengah);
+    }
+}
